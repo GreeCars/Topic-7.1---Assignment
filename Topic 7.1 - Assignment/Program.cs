e88f2a3e021afc1a2dc1688c5bfff278d0ee8835
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-03 09:38:27 -0400

    Working

--- Topic 7.1 - Assignment/Program.cs
+++ Topic 7.1 - Assignment/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Random generator = new Random();
+string userInput;
+int points = 3, randNum;
+bool quit = false;
+while (points > 0 && !quit)
+{
+    randNum = generator.Next(1, 2);
+    Console.WriteLine("You currently have " + points + " points.");
+    Console.WriteLine("Would you like to choose heads or tails, or do you want to quit?");
+    userInput = Console.ReadLine();
+    if (randNum == 1)
+    {
+        Console.WriteLine("Heads");
+        if (userInput.ToLower() == "heads")
+            points += 1;
+        else if (userInput.ToLower() == "tails")
+            points -= 1;
+    }
+    else if (randNum == 2)
