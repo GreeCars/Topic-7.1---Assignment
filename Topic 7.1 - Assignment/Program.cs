ba9279aec99ab2f6f1262be9f3e5aa1c218d49eb
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-04 09:38:15 -0400

    Working

--- Topic 7.1 - Assignment/Program.cs
+++ Topic 7.1 - Assignment/Program.cs
@@ -5,16 +5,45 @@ int points = 3, randNum;
 bool quit = false;
 while (points > 0 && !quit)
 {
-    randNum = generator.Next(1, 2);
+    randNum = generator.Next(1, 3);
     Console.WriteLine("You currently have " + points + " points.");
     Console.WriteLine("Would you like to choose heads or tails, or do you want to quit?");
+    Console.WriteLine();
     userInput = Console.ReadLine();
-    if (randNum == 1)
+    Console.WriteLine();
+    if (userInput.ToLower() == "heads")
     {
-        Console.WriteLine("Heads");
-        if (userInput.ToLower() == "heads")
+        if (randNum == 1)
+        {
+            Console.WriteLine("Heads");
             points += 1;
-        else if (userInput.ToLower() == "tails")
+        }
+        else if (randNum == 2)
+        {
+            Console.WriteLine("Tails");
             points -= 1;
+            if (points < 0)
+                Console.WriteLine("Sorry, you've run out of points.");
+        }
     }
-    else if (randNum == 2)
+    else if (userInput.ToLower() == "tails")
+    {
+        if (randNum == 2)
+        {
+            Console.WriteLine("Tails");
+            points += 1;
+        }
+        else if (randNum == 1)
+        {
+            Console.WriteLine("Heads");
+            points -= 1;
+            if (points < 0)
+                Console.WriteLine("Sorry, you've run out of points.");
+        }
+    }
+    else if (userInput.ToLower() == "quit")
+    {
+        quit = true;
+        Console.WriteLine("You ended the game with " + points + " points. Thanks for playing.");
+    }
+}
