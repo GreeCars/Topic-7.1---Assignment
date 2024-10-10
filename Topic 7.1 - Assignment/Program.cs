92d6008cb9de75f9c8d9167ee734156439bfdb06
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-10 08:47:13 -0400

    Finished

--- Topic 7.1 - Assignment/Program.cs
+++ Topic 7.1 - Assignment/Program.cs
@@ -3,28 +3,32 @@ Random generator = new Random();
 string userInput;
 int points = 3, randNum, bet;
 bool quit = false;
+Console.WriteLine("This is a coin game of heads or tails.");
 while (points > 0 && !quit)
 {
     randNum = generator.Next(1, 3);
     Console.WriteLine("You currently have " + points + " points.");
+    Console.WriteLine("How much would you like to bet? Do not bet more than you already have.");
+    Console.WriteLine();
+    Int32.TryParse(Console.ReadLine(), out bet);
     Console.WriteLine("Would you like to choose heads or tails, or do you want to quit?");
     Console.WriteLine();
     userInput = Console.ReadLine();
     Console.WriteLine();
-    Console.WriteLine("How much would you like to bet?");
-    Int32.TryParse(Console.ReadLine(), out bet);
-    if (userInput.ToLower() == "heads")
+    if (bet > points)
+        Console.WriteLine("Sorry, you can't bet that.");
+    else if (userInput.ToLower() == "heads")
     {
         if (randNum == 1)
         {
-            Console.WriteLine("Heads");
-            points += 1;
+            Console.WriteLine("Result: Heads");
+            points += bet;
         }
         else if (randNum == 2)
         {
-            Console.WriteLine("Tails");
-            points -= 1;
-            if (points == 0)
+            Console.WriteLine("Result: Tails");
+            points -= bet;
+            if (points <= 0)
                 Console.WriteLine("Sorry, you've run out of points.");
         }
     }
@@ -32,14 +36,14 @@ while (points > 0 && !quit)
     {
         if (randNum == 2)
         {
-            Console.WriteLine("Tails");
-            points += 1;
+            Console.WriteLine("Result: Tails");
+            points += bet;
         }
         else if (randNum == 1)
         {
-            Console.WriteLine("Heads");
-            points -= 1;
-            if (points == 0)
+            Console.WriteLine("Result: Heads");
+            points -= bet;
+            if (points <= 0)
                 Console.WriteLine("Sorry, you've run out of points.");
         }
     }
